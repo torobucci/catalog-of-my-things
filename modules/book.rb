3575644aebf07836803bdcf0e31ff85c84da6d7f
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2023-06-06 00:40:52 +0300

    Solve Rubocop issue

--- modules/book.rb
+++ modules/book.rb
@@ -1,6 +1,6 @@
 require_relative '../item'
 
-def class Book < Item
+class Book < Item
   attr_accessor :publisher, :cover_state
 
   def initialize(date, publisher, cover_state, archived: false, id: nil)
