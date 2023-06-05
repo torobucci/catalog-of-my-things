85ac6daf02f3769f1ba9ee6ee6a9ff8b1eff4d74
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2023-06-06 01:26:43 +0300

    create label class properties and constructor

--- modules/label.rb
+++ modules/label.rb
@@ -1,5 +1,12 @@
 require_relative '../item'
 
 class Label < Item
- 
+  attr_accessor :title, :author
+
+  def initialize(title, color, id: nil)
+    @id = id.nil? ? Random.rand(1..1000) : id
+    @title = title
+    @color = color
+    @items = []
+  end
 end
