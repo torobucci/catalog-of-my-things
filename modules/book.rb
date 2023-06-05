4b03baf7de14c8e8225019fe33d6ce48138750af
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2023-06-06 00:35:37 +0300

    Override class can_be_archived method

--- modules/book.rb
+++ modules/book.rb
@@ -2,10 +2,14 @@ require_relative '../item'
 
 def class Book < Item
   attr_accessor :publisher, :cover_state
-  
+
   def initialize(date, publisher, cover_state, archived: false, id: nil)
     super(id, date, archived: archived)
     @publisher = publisher
     @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || @cover_state == 'bad'
   end
 end
