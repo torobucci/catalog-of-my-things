0b438eb0148739af8525d6cd736ce73d450e7b70
torobucci <kevintoro254@gmail.com>
2023-06-05 18:38:00 +0300

    add game class and implement its methods

---
+++ game.rb
@@ -0,0 +1,15 @@
+require_relative './item'
+class Game < Item
+  def initialize(published_date, archived, multiplayer, last_played_at)
+    super(published_date, archived)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    current_date = Time.now
+    last_played_at = Time.parse(@last_played_at)
+    two_years_ago = Time.new(current_date.year - 2, current_date.month, current_date.day)
+    super() && last_played_at < two_years_ago
+  end
+end
