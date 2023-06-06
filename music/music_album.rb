b487b457ad196faae85ee3e820c685af5b3de5dc
Odongo006 <www.henryodongo006@gmail.com>
2023-06-06 18:58:31 +0300

    Define initialize method within a class

--- music/music_album.rb
+++ music/music_album.rb
@@ -2,4 +2,10 @@ require './item'
 
 class MusicAlbum < Item
     attr_accessor :on_spotify, :name
+
+    def initialize(name:, on_spotify:, publish_date:)
+        super(nil, nil, nil, nil, publish_date)
+        @on_spotify = on_spotify
+        @name = name
+    end
 end
