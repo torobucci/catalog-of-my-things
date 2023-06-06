9212ebbaa8fe23b899b1c26d2083afd007e2ccb6
torobucci <kevintoro254@gmail.com>
2023-06-06 23:02:46 +0300

    ensure app loads author and games data at start up

--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@ require_relative './app'
 require_relative './Modules/options'
 def main
   app = App.new
+  app.load_games
+  app.load_authors
   puts 'Welcome to catalog-of-my-things App!'
   options(app)
 end
