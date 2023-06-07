ca6b035741d09457332fbe42cc027ba4e729bdd8
Odongo006 <www.henryodongo006@gmail.com>
2023-06-07 15:31:17 +0300

    Use of snake case format5

--- music/music_album.rb
+++ music/music_album.rb
@@ -1,10 +1,10 @@
 require './item'
 
-class MusicAlbum < Item
+class Music_album < Item
   attr_accessor :on_spotify, :name
 
-  def initialize(name:, on_spotify:, publish_date:)
-    super(nil, nil, nil, nil, publish_date)
+  def initialize(name, on_spotify, publish_date, archived)
+    super(publish_date, archived)
     @on_spotify = on_spotify
     @name = name
   end
