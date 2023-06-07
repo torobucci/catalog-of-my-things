aba02d543fa80d6a770d58e2e2f38bc6d14b4679
Odongo006 <www.henryodongo006@gmail.com>
2023-06-07 14:42:24 +0300

    Remove extra spaces

--- music/music_album.rb
+++ music/music_album.rb
@@ -4,17 +4,17 @@ class MusicAlbum < Item
   attr_accessor :on_spotify, :name
 
   def initialize(name:, on_spotify:, publish_date:)
-      super(nil, nil, nil, nil, publish_date)
-      @on_spotify = on_spotify
-      @name = name
+    super(nil, nil, nil, nil, publish_date)
+    @on_spotify = on_spotify
+    @name = name
   end
 
   def add_genre=(genre)
-      @genre = genre
-      genre.items << self unless genre.items.include?(self)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
   end
 
   def can_be_archived?
-      super && @on_spotify
+    super && @on_spotify
   end
 end
