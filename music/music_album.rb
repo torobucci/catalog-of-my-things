2b15d6877e5b3c5e652ef7ce460864f6d16985ab
Odongo006 <www.henryodongo006@gmail.com>
2023-06-07 14:38:42 +0300

    Solve eslint errors

--- music/music_album.rb
+++ music/music_album.rb
@@ -1,20 +1,20 @@
 require './item'
 
 class MusicAlbum < Item
-    attr_accessor :on_spotify, :name
+  attr_accessor :on_spotify, :name
 
-    def initialize(name:, on_spotify:, publish_date:)
-        super(nil, nil, nil, nil, publish_date)
-        @on_spotify = on_spotify
-        @name = name
-    end
+  def initialize(name:, on_spotify:, publish_date:)
+      super(nil, nil, nil, nil, publish_date)
+      @on_spotify = on_spotify
+      @name = name
+  end
 
-    def add_genre=(genre)
-        @genre = genre
-        genre.items << self unless genre.items.include?(self)
-    end
+  def add_genre=(genre)
+      @genre = genre
+      genre.items << self unless genre.items.include?(self)
+  end
 
-    def can_be_archived?
-        super && @on_spotify
-    end
+  def can_be_archived?
+      super && @on_spotify
+  end
 end
