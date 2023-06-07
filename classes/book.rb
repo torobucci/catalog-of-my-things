f0ab04a989af8431398ab7fe6c25a1c7e3fc39fd
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2023-06-07 11:24:18 +0300

    Fix book class and apply reviewer requirements

---
+++ classes/book.rb
@@ -0,0 +1,19 @@
+require_relative '../item'
+
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publisher, cover_state, published_date, archived)
+    super(published_date, archived)
+    @id = rand(1..1000)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || @cover_state == "bad"
+  end
+end
+
+book = Book.new("Publisher", "good", "2021-01-01", false)
+p book.can_be_archived?
