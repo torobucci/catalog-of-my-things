d7deecb7421613ff20a8555d76d48837b3782949
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2023-06-08 12:24:56 +0300

    create schema for book and label classes

--- schema.sql
+++ schema.sql
@@ -15,3 +15,21 @@ CREATE TABLE author(
     first_name varchar,
     last_name varchar
 )
+
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  genre int REFERENCES genre(id),
+  author int REFERENCES author(id),
+  source int REFERENCES source(id),
+  label int REFERENCES label(id),
+  publisher VARCHAR(255),
+  cover_state VARCHAR(255),
+  published_date DATE,
+  archived BOOLEAN
+);
+
+CREATE TABLE labels (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  color VARCHAR(255)
+);
