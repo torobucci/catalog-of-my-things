0c2d9da2775c00329f4ea02501272e9fb8e60fdd
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2023-06-09 17:30:41 +0300

    Update test cases for book and label classes

--- Rspec/spec/book_spec.rb
+++ Rspec/spec/book_spec.rb
@@ -2,63 +2,34 @@ require_relative '../../item'
 require_relative '../../classes/book'
 
 RSpec.describe Book do
-  let(:published_date) { '2010-01-01' }
-  let(:archived) { false }
-  let(:publisher) { 'Publisher' }
-  let(:cover_state) { 'good' }
-  let(:book) { Book.new(publisher, cover_state, published_date, archived) }
+  
+  let(:published_date) { "2012-01-01" }
+  let(:publisher) { "Publisher" }
+  let(:cover_state) { "good" }
+  subject(:book) { described_class.new(publisher, cover_state, published_date) }
 
-  describe '#initialize' do
-    it 'sets the publisher' do
-      expect(book.publisher).to eq(publisher)
-    end
+  describe "#can_be_archived?" do
+    context "when the cover_state is good and the published_date is more than 10 years ago" do
+      let(:published_date) { (Time.now - (11 * 365 * 24 * 60 * 60)).to_s }
 
-    it 'sets the cover_state' do
-      expect(book.cover_state).to eq(cover_state)
-    end
-
-    it 'inherits the published_date attribute from the parent class' do
-      expect(book.published_date).to eq(published_date)
-    end
-
-    it 'inherits the archived attribute from the parent class' do
-      expect(book.archived).to eq(archived)
-    end
-  end
-
-  describe '#can_be_archived?' do
-    context 'when cover_state is bad' do
-      let(:cover_state) { 'bad' }
-
-      it 'returns true' do
-        expect(book.can_be_archived?).to eq(true)
+      it "returns true" do
+        expect(book.can_be_archived?).to be true
       end
     end
 
-    context 'when parent method returns true' do
-      let(:archived) { true }
+    context "when the cover_state is bad" do
+      let(:cover_state) { "bad" }
 
-      it 'returns true' do
-        expect(book.can_be_archived?).to eq(true)
-      end
-    end
-  end
-
-  describe '#move_to_archive' do
-    context 'when book can be archived' do
-      let(:archived) { false }
-
-      it 'sets archived to true' do
-        book.move_to_archive
-        expect(book.archived).to eq(true)
+      it "returns true" do
+        expect(book.can_be_archived?).to be true
       end
     end
 
-    context 'when book cannot be archived' do
-      let(:archived) { true }
+    context "when the cover_state is good and the published_date is within 10 years" do
+      let(:published_date) { (Time.now - (9 * 365 * 24 * 60 * 60)).to_s }
 
-      it 'does not change the value of archived' do
-        expect { book.move_to_archive }.not_to(change { book.archived })
+      it "returns false" do
+        expect(book.can_be_archived?).to be false
       end
     end
   end
--- Rspec/spec/label_spec.rb
+++ Rspec/spec/label_spec.rb
@@ -2,24 +2,29 @@ require_relative '../../item'
 require_relative '../../classes/label'
 
 RSpec.describe Label do
-  let(:title) { 'Label Title' }
-  let(:color) { 'Red' }
-  let(:label) { Label.new(title, color) }
+  let(:title) { "Label Title" }
+  let(:color) { "Label Color" }
+  subject(:label) { described_class.new(title, color) }
 
-  describe '#initialize' do
-    it 'sets the title' do
+  describe "#initialize" do
+    it "sets the title, color, id, and items" do
       expect(label.title).to eq(title)
+      expect(label.color).to eq(color)
+      expect(label.id).to be_between(1, 1000).inclusive
+      expect(label.items).to be_a(Array)
     end
   end
 
-  describe '#add_item' do
-    let(:item) { Item.new('2020-01-01', false) }
+  describe "#add_item" do
+    let(:item) { Item.new("2022-01-01") }
 
-    it 'adds the item to the label' do
-      expect { label.add_item(item) }.to change { label.instance_variable_get(:@items).length }.by(1)
+    it "adds the item to the items array" do
+      expect {
+        label.add_item(item)
+      }.to change { label.items.length }.by(1)
     end
 
-    it 'sets the label attribute of the item' do
+    it "sets the label of the added item to self" do
       label.add_item(item)
       expect(item.label).to eq(label)
     end
