9c3cdf4557f048f78b3f51370ca6e8278fad8c85
torobucci <kevintoro254@gmail.com>
2023-06-09 11:47:35 +0300

    add id instance and to_json and from_json methods

--- music/genre.rb
+++ music/genre.rb
@@ -1,9 +1,8 @@
-require_relative '../item'
-
 class Genre
-  attr_accessor :name, :items
+  attr_accessor :name, :items, :id
 
   def initialize(name)
+    @id = rand(1..100)
     @name = name
     @items = []
   end
@@ -11,5 +10,19 @@ class Genre
   def add_item(item)
     item.genre = self
     @items << item
+  end
+
+  def to_json(*_args)
+    {
+      'id' => @id,
+      'name' => @name
+    }.to_json
+  end
+
+  def self.from_json(json)
+    data = JSON.parse(json)
+    genre = new(data['name'])
+    genre.id = data['id'].to_i
+    genre
   end
 end
